2f5f2daecfdd119c652273f315b0364fbd54ecc9
cfxxldev <47990291+cfxxldev@users.noreply.github.com>
2020-06-19 22:22:31 +0200

    improved handling of UTF-8 in names

--- P4GPC_Namechanger/P4GPC_Namechanger.cpp
+++ P4GPC_Namechanger/P4GPC_Namechanger.cpp
@@ -17,6 +17,8 @@
 #include <thread>
 #include <iostream>
 #include <algorithm> 
+#include <fcntl.h>
+#include <io.h>
 
 namespace fs = std::filesystem;
 void backup(fs::path binslot_file)
@@ -37,7 +39,7 @@ void backup(fs::path binslot_file)
 		std::stringstream strExtension;
 		strExtension << std::put_time(std::localtime(&time), "%Y%m%d_%H%M%S") << binslot_file.extension().generic_string();
 		backup_file.replace_extension(strExtension.str());
-		std::cout << "Creating backup: " << backup_file.make_preferred().string() << "\n";
+		std::wcout << L"Creating backup: " << backup_file.make_preferred().wstring() << "\n";
 		fs::copy_file(binslot_file, backup_file, fs::copy_options::overwrite_existing);
 		}
 	}
@@ -56,31 +58,47 @@ bool separate_console(void)
 	return ((!csbi.dwCursorPosition.X) && (!csbi.dwCursorPosition.Y));
 	}
 
-static inline void ltrim(std::string& s)
+static inline void ltrim(std::wstring& s)
 	{
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char c)
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](wchar_t c)
 									{
-									return !isspace(c);
+									return !iswspace(c);
 									}));
 	}
 
-static inline void rtrim(std::string& s)
+static inline void rtrim(std::wstring& s)
 	{
-	s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char c)
+	s.erase(std::find_if(s.rbegin(), s.rend(), [](wchar_t c)
 						 {
-						 return !isspace(c) && c != 0;
+						 return !iswspace(c) && c != 0;
 						 }).base(), s.end());
 	}
 
-int main(int argc, char* argv[])
+template <size_t size>
+int utf16_to_utf8(const wchar_t* src, char (&buffer)[size])
+	{
+	return utf16_to_utf8(src, buffer, size);
+	}
+
+int utf16_to_utf8(const wchar_t* src, char* dst, size_t buffer_size)
+	{
+	int nBytes = WideCharToMultiByte(CP_UTF8, WC_COMPOSITECHECK, src, -1, dst, buffer_size - 1, nullptr, nullptr);
+	if(nBytes >= 0)	dst[nBytes] = 0;
+	return nBytes;
+	}
+
+int wmain(int argc, wchar_t* argv[])
 	{
 	if (argc <= 0)
 		return -1;
 
 	bool wait_for_key = separate_console();
 
-	std::cout << "Persona 4 Golden PC namechanger" << '\n';
-	std::cout << "Copyright (c) 2020 Andreas Gebert" << "\n\n";
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	_setmode(_fileno(stdin), _O_U16TEXT);
+
+	std::wcout << L"Persona 4 Golden PC namechanger" << L"\n";
+	std::wcout << L"Copyright (c) 2020 Andreas Gebert" << L"\n\n";
 
 	fs::path exe_file(argv[0]);
 
@@ -99,19 +117,17 @@ int main(int argc, char* argv[])
 	fs::path bin_file(argv[1]);
 	if (bin_file.extension() == ".bin" && fs::exists(bin_file))
 		{
-		std::string lastname	= (argc>=3)?argv[2]:"";
-		std::string firstname	= (argc>=4)?argv[3]:"";
+		std::wstring lastname	= (argc>=3)?argv[2]:L"";
+		std::wstring firstname	= (argc>=4)?argv[3]:L"";
 		if (lastname.empty())
 			{
-			std::cout << "please enter last name: ";
-			std::getline(std::cin, lastname);
-//			std::cin >> lastname;
+			std::wcout << L"please enter last name: ";
+			std::getline(std::wcin, lastname);
 			}
 		if (firstname.empty())
 			{
-			std::cout << "please enter first name: ";
-			std::getline(std::cin, firstname);
-//			std::cin >> firstname;
+			std::wcout << L"please enter first name: ";
+			std::getline(std::wcin, firstname);
 			}
 
 		// limit length to 8 Characters
@@ -122,7 +138,7 @@ int main(int argc, char* argv[])
 
 		backup(bin_file);
 		
-		std::cout << "Setting character name to " << lastname << " " << firstname << "\n";
+		std::wcout << L"Setting character name to " << lastname << " " << firstname << L"\n";
 		FILE* fBin = nullptr;
 		if (_wfopen_s(&fBin, bin_file.c_str(), L"r+b") == 0 && fBin)
 			{
@@ -130,24 +146,24 @@ int main(int argc, char* argv[])
 			char firstname_buffer[16];
 			memset(lastname_buffer, 0, sizeof lastname_buffer);
 			memset(firstname_buffer, 0, sizeof firstname_buffer);
-			strcpy_s(lastname_buffer, lastname.c_str());
-			strcpy_s(firstname_buffer, firstname.c_str());
+			utf16_to_utf8(lastname.c_str(), lastname_buffer);
+			utf16_to_utf8(firstname.c_str(), firstname_buffer);
 			// These locations are used by the PS-Vita version, the PC version updated these locations but ignores them otherwise
 			if (fseek(fBin, 0x00010, SEEK_SET) == 0)
 				{
-				fwrite(lastname_buffer, 1, 8, fBin);
+				fwrite(lastname_buffer, 1, 16, fBin);
 				}
 			if (fseek(fBin, 0x00022, SEEK_SET) == 0)
 				{
-				fwrite(firstname_buffer, 1, 8, fBin);
+				fwrite(firstname_buffer, 1, 16, fBin);
 				}
 			if (fseek(fBin, 0x00064, SEEK_SET) == 0)
 				{
-				fwrite(lastname_buffer, 1, 8, fBin);
+				fwrite(lastname_buffer, 1, 16, fBin);
 				}
 			if (fseek(fBin, 0x00076, SEEK_SET) == 0)
 				{
-				fwrite(firstname_buffer, 1, 8, fBin);
+				fwrite(firstname_buffer, 1, 16, fBin);
 				}
 
 			// Calculate the checksum over the first 54 Bytes and save it at offset 0x00036(54)
@@ -181,19 +197,19 @@ int main(int argc, char* argv[])
 
 			if (fseek(fBin, 0x015130, SEEK_SET) == 0)
 				{
-				fwrite(lastname_buffer, 1, 8, fBin);
+				fwrite(lastname_buffer, 1, 16, fBin);
 				}
 			if (fseek(fBin, 0x015142, SEEK_SET) == 0)
 				{
-				fwrite(firstname_buffer, 1, 8, fBin);
+				fwrite(firstname_buffer, 1, 16, fBin);
 				}
 			if (fseek(fBin, 0x019CEC, SEEK_SET) == 0)
 				{
-				fwrite(lastname_buffer, 1, 8, fBin);
+				fwrite(lastname_buffer, 1, 16, fBin);
 				}
 			if (fseek(fBin, 0x019CFE, SEEK_SET) == 0)
 				{
-				fwrite(firstname_buffer, 1, 8, fBin);
+				fwrite(firstname_buffer, 1, 16, fBin);
 				}
 
 			fclose(fBin);
@@ -209,15 +225,15 @@ int main(int argc, char* argv[])
 			}
 		else
 			{
-			std::cout << "Please make sure update the checksum in your dataXXXX.binslot file.\n";
+			std::wcout << L"Please make sure update the checksum in your dataXXXX.binslot file.\n";
 			}
 		}
 
 	// Ignore to the end of file
-	std::cin.clear();
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::wcin.clear();
+	std::wcin.ignore(std::numeric_limits<std::streamsize>::max(), L'\n');
 
 	if (wait_for_key)
-		getchar();
+		getwchar();
 	return 0;
 	}
