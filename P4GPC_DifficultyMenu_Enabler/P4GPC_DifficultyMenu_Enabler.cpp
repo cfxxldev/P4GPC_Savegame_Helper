965cd616507b77e6a8e6f4fd024f0358f094f32b
cfxxldev <47990291+cfxxldev@users.noreply.github.com>
2020-06-27 21:32:23 +0200

    Added new tool P4GPC_DifficultyMenu_Enabler

--- P4GPC_DifficultyMenu_Enabler/P4GPC_DifficultyMenu_Enabler.cpp
+++ P4GPC_DifficultyMenu_Enabler/P4GPC_DifficultyMenu_Enabler.cpp
@@ -58,35 +58,6 @@ bool separate_console(void)
 	return ((!csbi.dwCursorPosition.X) && (!csbi.dwCursorPosition.Y));
 	}
 
-static inline void ltrim(std::wstring& s)
-	{
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](wchar_t c)
-									{
-									return !iswspace(c);
-									}));
-	}
-
-static inline void rtrim(std::wstring& s)
-	{
-	s.erase(std::find_if(s.rbegin(), s.rend(), [](wchar_t c)
-						 {
-						 return !iswspace(c) && c != 0;
-						 }).base(), s.end());
-	}
-
-template <size_t size>
-int utf16_to_utf8(const wchar_t* src, char (&buffer)[size])
-	{
-	return utf16_to_utf8(src, buffer, size);
-	}
-
-int utf16_to_utf8(const wchar_t* src, char* dst, size_t buffer_size)
-	{
-	int nBytes = WideCharToMultiByte(CP_UTF8, WC_COMPOSITECHECK, src, -1, dst, buffer_size - 1, nullptr, nullptr);
-	if(nBytes >= 0)	dst[nBytes] = 0;
-	return nBytes;
-	}
-
 int wmain(int argc, wchar_t* argv[])
 	{
 	if (argc <= 0)
@@ -97,15 +68,16 @@ int wmain(int argc, wchar_t* argv[])
 	_setmode(_fileno(stdout), _O_U16TEXT);
 	_setmode(_fileno(stdin), _O_U16TEXT);
 
-	std::wcout << L"Persona 4 Golden PC namechanger" << L"\n";
+	std::wcout << L"Persona 4 Golden PC Difficulty Menu Enabler" << L"\n";
 	std::wcout << L"Copyright (c) 2020 Andreas Gebert" << L"\n\n";
 
 	fs::path exe_file(argv[0]);
 
 	if (argc == 1)
 		{
-		wprintf(L"usage: %s dataXXXX.bin [lastname [firstname]]\n", exe_file.filename().c_str());
+		wprintf(L"usage: %s dataXXXX.bin\n", exe_file.filename().c_str());
 		wprintf(L"You can also just drag and drop a dataXXXX.bin file on this program.\n");
+
 		if (wait_for_key)
 			getwchar();
 		return 0;
@@ -117,55 +89,11 @@ int wmain(int argc, wchar_t* argv[])
 	fs::path bin_file(argv[1]);
 	if (bin_file.extension() == ".bin" && fs::exists(bin_file))
 		{
-		std::wstring lastname	= (argc>=3)?argv[2]:L"";
-		std::wstring firstname	= (argc>=4)?argv[3]:L"";
-		if (lastname.empty())
-			{
-			std::wcout << L"please enter last name: ";
-			std::getline(std::wcin, lastname);
-			}
-		if (firstname.empty())
-			{
-			std::wcout << L"please enter first name: ";
-			std::getline(std::wcin, firstname);
-			}
-
-		// limit length to 8 Characters
-		lastname.resize(8);
-		firstname.resize(8);
-		rtrim(lastname);
-		rtrim(firstname);
-
 		backup(bin_file);
 		
-		std::wcout << L"Setting character name to " << lastname << " " << firstname << L"\n";
 		FILE* fBin = nullptr;
 		if (_wfopen_s(&fBin, bin_file.c_str(), L"r+b") == 0 && fBin)
 			{
-			char lastname_buffer[16];
-			char firstname_buffer[16];
-			memset(lastname_buffer, 0, sizeof lastname_buffer);
-			memset(firstname_buffer, 0, sizeof firstname_buffer);
-			utf16_to_utf8(lastname.c_str(), lastname_buffer);
-			utf16_to_utf8(firstname.c_str(), firstname_buffer);
-			// These locations are used by the PS-Vita version, the PC version updated these locations but ignores them otherwise
-			if (fseek(fBin, 0x00010, SEEK_SET) == 0)
-				{
-				fwrite(lastname_buffer, 1, 16, fBin);
-				}
-			if (fseek(fBin, 0x00022, SEEK_SET) == 0)
-				{
-				fwrite(firstname_buffer, 1, 16, fBin);
-				}
-			if (fseek(fBin, 0x00064, SEEK_SET) == 0)
-				{
-				fwrite(lastname_buffer, 1, 16, fBin);
-				}
-			if (fseek(fBin, 0x00076, SEEK_SET) == 0)
-				{
-				fwrite(firstname_buffer, 1, 16, fBin);
-				}
-
 			// Calculate the checksum over the first 54 Bytes and save it at offset 0x00036(54)
 			// this checksum is the reason the game crashes after using the Savegame editor to change the name
 			if (fseek(fBin, 0x00000, SEEK_SET) == 0)
@@ -187,31 +115,24 @@ int wmain(int argc, wchar_t* argv[])
 					}
 				}
 
-			// The PC uses these locations
-			if (fseek(fBin, 0x019CE8, SEEK_SET) == 0)
+			// This flag controls the availability of the Difficulty menu ( and also most likely ng+ status)
+			// observed values:
+			//	00 - normal new game on vita
+			//	01 - ng+ on vita (probably PC also)
+			//	02 - normal new game on PC
+			if (fseek(fBin, 0x01304, SEEK_SET) == 0)
 				{
-				uint8_t flag = 1;	// 0 = always use default name 
-									// 1 = use character name from savefile
-				fwrite(&flag, 1, 1, fBin);
+				uint8_t flag[1];
+				auto nCount = fread_s(flag, sizeof flag, sizeof uint8_t, _countof(flag), fBin);
+				if (nCount == _countof(flag) && flag[0] == 0x00)
+					{
+					flag[0] = 0x02;
+					if (fseek(fBin, 0x01304, SEEK_SET) == 0)
+						{
+						fwrite(flag, sizeof uint8_t, _countof(flag), fBin);
+						}
+					}
 				}
-
-			if (fseek(fBin, 0x015130, SEEK_SET) == 0)
-				{
-				fwrite(lastname_buffer, 1, 16, fBin);
-				}
-			if (fseek(fBin, 0x015142, SEEK_SET) == 0)
-				{
-				fwrite(firstname_buffer, 1, 16, fBin);
-				}
-			if (fseek(fBin, 0x019CEC, SEEK_SET) == 0)
-				{
-				fwrite(lastname_buffer, 1, 16, fBin);
-				}
-			if (fseek(fBin, 0x019CFE, SEEK_SET) == 0)
-				{
-				fwrite(firstname_buffer, 1, 16, fBin);
-				}
-
 			fclose(fBin);
 			}
 
